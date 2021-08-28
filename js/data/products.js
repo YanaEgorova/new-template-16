01503af790deb084acb4cacb2033645d3d4a661a
Yana <yanaegorova69.3@gmail.com>
2021-08-29 01:00:35 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,146 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Cable Fastening Tape',
+        price: 6.96,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Double-sided allows you to cut custom-length rings to wrap unsightly cables together.',
+            },
+            {
+                text: 'Size: 3/4-Inch width x 25 Yards,Cable Management is much easier with tie roll.',
+            },
+            {
+                text: 'Easy to Install, reusable Hook and Loop Material.',
+            },
+            {
+                text: 'High quality magic tape for you to tie cables or other fastening/attaching needs.',
+            },
+            {
+                text: 'Easy to cut to make customizable length cable ties.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png', './img/img2-5.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Water Proof Leg Cast Cover',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Ideal for the WATERTIGHT PROTECTION of casts, bandages, rashes, abrasions, burns, cuts, wounds, IV & PICC Lines.',
+            },
+            {
+                text: 'REUSABLE and QUALITY made. The cast cover is made of a heavy duty vinyl that can be reused for approximately 6-8 weeks.',
+            },
+            {
+                text: 'EASY APPLICATION. It is easy to put on, just slide it up and over your foot until the seal makes contact with your skin.',
+            },
+            {
+                text: 'Designed to fit EVERYONE in the family. Unique diaphragm allows for custom, secure fit around your leg.',
+            },
+            {
+                text: 'Made with NON-LATEX materials. The "seal" is made from a non-latex material and stretches easy to accommodates most size legs and casts.',
+            },
+        ],
+        images: ['./img/img4-1.jpg', './img/img4-2.png' , './img/img4-3.png', './img/img4-4.jpg'],
+    },
+    {
+        id: 'product-3',
+        name: 'Wood Folding Straight Razor',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Stainless Steel Body; Enables A Smooth Shave; Resists Heat And Chemicals And Other Impurities;  Razors Body Is The Cleanest And Most Hygienic Of Materials; Stainless Steel Is Autoclave And Disinfectant Safe.',
+            },
+            {
+                text: 'Super Resin Handle; Provides Excellent Grip And Control; The Enamel Handle Is Made From A Super Engineered Resin; Resists Heat Up To One Hundred Thirty Five Degrees Celsius Or Two Hundred Seventy Five Degrees Fahrenheit.',
+            },
+            {
+                text: 'Blade Replacements; Changing The Blade Is Fast And Easy; Pinch The Razor Head And The Blade Will Drop Out.',
+            },
+            {
+                text: 'Spring Mounted Blade Head; Spring Mounted And One Touch Mechanism; Allows Razor To Easily Be Dismantled And Reassembled For Cleaning.',
+            },
+            {
+                text: 'Specifications; Scotch Wood Handle Is Four Point Eight Inches; Stainless Steel Body Is Five Point Six Inches; Weighs Fifty One Grams.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.jpg', './img/img5-4.jpg'],
+    },
+    {
+        id: 'product-4',
+        name: 'Razor and Shaving Brush Stand',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Material',
+                text: '304 Stainless Steel, solid and durable. Weighted Bottom makes it sturdy and elegant. With bowl in matte black.',
+            },
+            {
+                title: 'Stylish design',
+                text: 'If you are looking for a functional wet shaving razor brush stand, this one will be an ideal choice!',
+            },
+            {
+                title: 'Ideal size',
+                text: 'Suitable for most shaving brush and razor. But if it cannot fit your razor or brush, you can return and get your money back.',
+            },
+            {
+                title: 'Fair price but high quality',
+                text: 'Want to buy a classical shaving accessory but do not want to spend a lot? Just start from this one.',
+            },
+            {
+                title: '100% satisfaction guarantee',
+                text: 'If it doesn\'t work properly, or you are not happy with the product, please contact us and we will make it right! You will get a refund or a new one for free.',
+            },
+        ],
+        images: ['./img/img6-1.jpg', './img/img6-2.png', './img/img6-3.jpg'],
+    },
+    {
+        id: 'product-5',
+        name: 'Bodyweight Resistance System',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'All inclusive package',
+                text: 'Includes Suspension Training Strap, Suspension Anchor, Door Anchor, best-in-Class 35 page full-color Workout Guide.',
+            },
+            {
+                title: 'One solution … many benefits',
+                text: 'Builds muscle, Burns Fat, Increases Endurance & Improves Flexibility.',
+            },
+            {
+                title: 'Quick and easy setup',
+                text: 'Simple and easy setup allows the Suspension Trainer to be set up in 60 seconds. Anchor off any door, tree branch, rafter, beam or secured elevation point.',
+            },
+            {
+                title: 'On the road, or on the go',
+                text: 'Get professional results anywhere, any time. The Training Suspension Trainer Basic Kit with Anchor is portable and convenient for travel.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Towel Warmer',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Ultra large capacity',
+                text: 'The ultra-large insulated plastic bucket design accommodates up to two 40’’ x 70’’ oversized bath towels, robes, throw blanket, PJ’s and more.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.jpg', './img/img1-5.png', './img/img1-6.png'],
+    },
+]
